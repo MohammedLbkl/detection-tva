a3528bdc701ccb3b50590bc414eba0dedd6ef7c5
MohammedLbakali <mohammedlbakali@MacBook-Air-de-MohammedLbakali.local>
2025-10-25 09:06:13 +0200

    Test Yolo with caméra

---
+++ src/detection/yolov8.py
@@ -0,0 +1,25 @@
+from ultralytics import YOLO
+
+
+
+MODEL_PATH = "src/detection/model/CornerDocument.pt"
+model = YOLO(MODEL_PATH)
+
+
+SOURCE = 0  
+
+try:
+    print("Your Iphone14 pro max 512GO is activate")
+    for result in model.predict(
+        source=SOURCE,
+        show=True,        # Affiche la vidéo avec les bounding boxes
+        stream=True,      
+        conf=0.7,         
+        verbose=False
+    ):
+        pass 
+
+except KeyboardInterrupt:
+    print("Ctrl+C is press.")
+finally:
+    print("End.")
